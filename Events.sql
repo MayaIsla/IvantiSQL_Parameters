9b138ae896050ec6ac3a448487d399b843d8b6b5
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2024-02-16 10:28:16 -0600

    Add files via upload

---
+++ Events.sql
@@ -0,0 +1,28 @@
+SELECT  TOP 1  ServiceReq.CreatedDateTime, ProfileFullName as Customer, Status as ServiceRequestStatus,ServiceReqTemplateParam.DisplayName,
+ParameterValue, ServiceRequestParameters.*
+FROM ServiceReq 
+INNER JOIN ServiceReqTemplateParam ON (ServiceReq.SvcReqTmplLink_RecID = ServiceReqTemplateParam.ParentLink_RecID)
+LEFT OUTER JOIN ServiceReqParam ON (
+  ServiceReqParam.ParentLink_RecID = ServiceReq.RecId
+  AND ServiceReqParam.ParameterName = ServiceReqTemplateParam.NAME
+  )
+--LEFT JOIN [FusionAttachments] att ON att.RecID = ServiceReqParam.ParameterValue
+OUTER APPLY (
+	SELECT [Requester]
+				,[employee_list_textarea] as "employee list"
+				,[Requester_Title]
+	FROM (
+		SELECT ParameterValue
+			,ParameterName
+		FROM ServiceReqParam srp with (NOLOCK)
+		WHERE srp.ParentLink_RecID = servicereq.RecID
+		) AllParams
+	pivot(max(ParameterValue) FOR ParameterName IN (
+				[Requester]
+				,[employee_list_textarea] 
+				,[requester_title]
+				)) PivotedParams
+	) ServiceRequestParameters
+	LEFT JOIN [Frs_def_validation_lists] val ON val.RecID = ServiceReqTemplateParam.ValidationList_RecID
+WHERE (ServiceReqParam.ParentLink_RecID = ('98022849CE194109B68035BB29A75843')) AND (ParameterValue IS NOT NULL) AND (ParameterValue NOT LIKE '%<%') AND (ServiceReqTemplateParam.DisplayName IS NOT NULL)
+ORDER BY ServiceReqTemplateParam.SequenceNum
